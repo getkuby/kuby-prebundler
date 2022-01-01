1f3afd6bac44fbbafb345a9c9f6dec52004ee060
Cameron Dutro <camertron@gmail.com>
2021-12-31 21:58:48 -0800

    Hmm

--- lib/kuby/prebundler/prebundler_phase.rb
+++ lib/kuby/prebundler/prebundler_phase.rb
@@ -24,9 +24,9 @@ module Kuby
         dockerfile.arg('PREBUNDLER_SECRET_ACCESS_KEY')
 
         dockerfile.copy(prebundle_config || '.prebundle_config', '.')
-        # dockerfile.run('gem', 'install', 'prebundler', '-v', "'< 1'")
-        dockerfile.run('gem', 'install', 'specific_install')
-        dockerfile.run('gem', 'specific_install', '-l', 'https://github.com/camertron/prebundler.git', '-b', 'eval_gemfile')
+        dockerfile.run('gem', 'install', 'prebundler', '-v', "'< 1'")
+        # dockerfile.run('gem', 'install', 'specific_install')
+        # dockerfile.run('gem', 'specific_install', '-l', 'https://github.com/camertron/prebundler.git', '-b', 'eval_gemfile')
         dockerfile.env('GLI_DEBUG', 'true')
 
         bundler_phase.apply_to(dockerfile)
